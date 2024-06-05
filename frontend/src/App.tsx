17d6e96c867d30b8538240108c5bb8d72c03eaa1
dpak33 <105285976+dpak33@users.noreply.github.com>
2024-06-05 08:27:11 -0700

    "min styling"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import './App.css';
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import SignUp from './auth/SignUp';
