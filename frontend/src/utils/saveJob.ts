8e3bae7130dd65235ae7c9021492b1512f924e54
dpak33 <105285976+dpak33@users.noreply.github.com>
2024-06-09 16:15:26 -0700

    y

--- frontend/src/utils/saveJob.ts
+++ frontend/src/utils/saveJob.ts
@@ -3,7 +3,7 @@ import { Job } from '../types';
 
 export const saveJob = async (job: Job) => {
     try {
-        await axios.post('http://localhost:8000/jobs/save-job', job);
+        await axios.post(`${process.env.REACT_APP_BACKEND_URL}/jobs/save-job`, job);
         alert('Job saved successfully');
     } catch (error) {
         alert('Failed to save job');
